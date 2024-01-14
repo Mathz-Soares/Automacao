0b58f67071f7a3051f4d5310f98d8c0c973b17f2
Mathz-Soares <matheussou487@gmail.com>
2024-01-14 12:10:09 -0300

    completo

--- codigo.py
+++ codigo.py
@@ -21,5 +21,30 @@ time.sleep(1)
 senha = "12345"
 py.write(senha)
 time.sleep(2)
-py.press("tab")
-py.press("enter")
+py.click(x=694, y=557)
+time.sleep(3)
+
+import pandas
+tabela = pandas.read_csv("produtos.csv")
+
+for linha in tabela.index:
+    py.click(x=556, y=282)
+    py.write(tabela.loc[linha, "codigo"])
+    py.press("tab")
+    py.write(tabela.loc[linha, "marca"])
+    py.press("tab")
+    py.write(tabela.loc[linha, "tipo"])
+    py.press("tab")
+    py.write(str(tabela.loc[linha, "categoria"]))
+    py.press("tab")
+    py.write(str(tabela.loc[linha, "preco_unitario"]))
+    py.press("tab")
+    py.write(str(tabela.loc[linha, "custo"]))
+    py.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+        py.write(obs)
+    py.press("tab")
+    py.press("enter")
+    py.scroll(1000)
+
---
+++ teste.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time
+
+time.sleep(3)
+pyautogui.click(x=514, y=435, clicks=10000)
